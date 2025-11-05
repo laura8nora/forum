e112311b97782030bdf2b076b75770859c366da7
laura8nora <lauranora.gyulai@skola.taby.se>
2025-11-05 15:01:52 +0100

    Create users.sql

---
+++ users.sql
@@ -0,0 +1,15 @@
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+CREATE TABLE `users` (
+  `username` varchar(10) NOT NULL,
+  `password` varchar(20) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `id` int(5) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+INSERT INTO `users` (`username`, `password`, `email`, `id`) VALUES
+('alexander', 'lukas', 'alexander.lukas@skola.taby.se', 1)
+('john', 'smith', 'john.smith@skola.taby.se', 2),
+('jim', 'davis', 'jim.davis@skola.taby.se', 3);
