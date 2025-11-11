9a5258350e349c19e64b82492b2592562d7478f6
laura8nora <lauranora.gyulai@skola.taby.se>
2025-11-11 13:05:19 +0100

    Update forum.py

--- forum.py
+++ forum.py
@@ -1,15 +1,14 @@
 from flask import Flask, render_template, session, redirect, url_for, request
-import mysql.connector  # installera med "pip install mysql-connector-python" i kommandotolken, ifall du inte redan gjort detta
+import mysql.connector
 
 app = Flask(__name__)
-app.secret_key = 'hemligtextsträngsomingenkangissa'  # Används för sessionshantering
+app.secret_key = 'hemligtextsträngsomingenkangissa'
 
-# skapa databasuppkoppling
 def get_connection(host="localhost", user="root", password=""):
     mydb = mysql.connector.connect(
         host=host,
         user=user,
         password=password,
-        database="webbserverprogrammering"  # byt namn om din databas heter något annat
+        database="webbserverprogrammering"
     )
     return mydb
